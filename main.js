30e837411ce18a2a37d67a09e6466b03de6866db
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-02-26 21:17:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let rl = Readline.createInterface(process.stdin, process.stdout)
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6289670394574','6285823554146','6285747874973'] // Put your number here
+global.owner = ['6289670394574','6285823554146','6285747874973','6285762005157'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
