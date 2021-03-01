791199b39401242d8d99da27f0e56e116ce2e0cf
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-03-01 19:37:19 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,7 +5,7 @@ let handler = function (m) {
  this.sendContact(m.chat, '6285823554146', '》Wilka_Kenap《', m),
  this.sendContact(m.chat, '6285762005157', 'Trian', m)
  this.sendContact(m.chat, '6281319352377', 'lord', m)
- this.sendContact(m.chat, '6282273025238', 'Hiro', m)
+ this.sendContact(m.chat, '6282273025238', '『HIRO』༺½༻', m)
  this.sendContact(m.chat, '6285240424044', 'Mahesa :v', m)
  this.sendContact(m.chat, '6289505289896', 'Calvin', m)
  this.sendContact(m.chat, '62895433996700', 'Odin/Hashira', m)
