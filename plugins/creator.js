99873a9637ae47c5efad341c921cf3e67ef71a85
lastsymphony <78311812+lastsymphony@users.noreply.github.com>
2021-03-01 19:16:08 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,12 +3,13 @@ let handler = function (m) {
   this.sendContact(m.chat, '6289670394574', 'Aditya jatayu', m),
  this.sendContact(m.chat, '6285747874973', 'Kemal Ibnu', m),
  this.sendContact(m.chat, '6285823554146', '》Wilka_Kenap《', m),
- this.sendContact(m.chat, '6285762005157', 'No Name', m)
+ this.sendContact(m.chat, '6285762005157', 'Trian', m)
  this.sendContact(m.chat, '6281319352377', 'lord', m)
  this.sendContact(m.chat, '6282273025238', 'Hiro', m)
- this.sendContact(m.chat, '6285240424044', 'Zen', m)
+ this.sendContact(m.chat, '6285240424044', 'Mahesa :v', m)
  this.sendContact(m.chat, '6289505289896', 'Calvin', m)
- this.sendContact(m.chat, '62895433996700', 'Nothing', m)
+ this.sendContact(m.chat, '62895433996700', 'Odin/Hashira', m)
+  this.sendContact(m.chat, '62895618689182', 'Ade', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
